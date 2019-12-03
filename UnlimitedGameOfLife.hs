4d01d0a0e81fdbf10efd1eef4f873ca7d7a63c63
Nithin Muthukumar <kathrikai30201094@gmail.com>
2019-12-02 22:13:33 -0500

    doing unlimitedConwaysGameOfLife

--- UnlimitedGameOfLife.hs
+++ UnlimitedGameOfLife.hs
@@ -1,13 +1,24 @@
 module UnlimitedGameOfLife where
 
-genGrid :: [(Int,Int)] -> [[Int]]
-genGrid cells = 
+getRangeX :: [(Int,Int)]-> [Int]
+getRangeX cells= [minimum ls.. maximum ls] where ls=[x | (x,y) <- cells]
+getRangeY :: [(Int,Int)] ->[Int]
+getRangeY cells= [minimum ls.. maximum ls] where ls=[y | (x,y) <- cells]
+genGrid :: [(Int,Int)] ->Int-> [[Int]]
+genGrid cells=[[if (x,y) `elem` cells then 1 else 0 | x<-getRangeX cells]|y<-getRangeY cells]
+ 
+--finish life function
+life :: [(Int,Int)] -> (Int,Int) -> Int
+life cells pos=  
+
+step :: [(Int,Int)]->[(Int,Int)]
+step cells = [life cells (x,y) | x<-getRangeX cells,y<-getRangeY cells]   
 
 getCells :: [[Int]]-> [(Int,Int)]
 getCells grid = [(x,y) | x<-length grid!!0, y<- length grid,grid!!y!!x==1]
 
 gol :: [(Int,Int)] -> Int -> [[Int]]
-gol cells n = gol [] n-1
+gol cells n = gol (step cells) n-1
 gol cells 0 = genGrid cells
 
 getGeneration :: [[Int]] -> Int -> [[Int]]
