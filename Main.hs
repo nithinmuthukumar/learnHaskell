fa982ccbc0b5f4b2f17930a165fcf4a7984fec78
Nithin Muthukumar <kathrikai30201094@gmail.com>
2019-12-06 23:47:28 -0500

    added longestPalindrome and Matrix Mul

--- Main.hs
+++ Main.hs
@@ -1,11 +1,22 @@
 module Main where
 import MazeRunner
 import Collatz
+import UnlimitedGameOfLife
+import LongestPalindrome
 
 spinWords :: String -> String
 spinWords str = unwords [if length x<5 then x else reverse x| x<- words str ]
 
+grid :: [[Int]]
+grid= [
+    [1,0,0,1,1,1,0,1],
+    [1,0,1,1,0,0,1,1],
+    [1,1,0,0,1,1,0,1],
+    [1,0,1,0,1,0,1,0],
+    [1,1,1,1,1,0,1,0]]
 main :: IO ()
 main = do
-    putStrLn ( collatz 3)
+    --putStrLn $ show $ getGeneration grid 1
+    putStrLn $ show $ longestPalindrome "baablkj12345432133d"
+    
     
--- UnlimitedGameOfLife.hs
+++ UnlimitedGameOfLife.hs
@@ -2,25 +2,35 @@ module UnlimitedGameOfLife where
 
 getRangeX :: [(Int,Int)]-> [Int]
 getRangeX cells= [minimum ls.. maximum ls] where ls=[x | (x,y) <- cells]
+
 getRangeY :: [(Int,Int)] ->[Int]
 getRangeY cells= [minimum ls.. maximum ls] where ls=[y | (x,y) <- cells]
-genGrid :: [(Int,Int)] ->Int-> [[Int]]
+
+genGrid :: [(Int,Int)]-> [[Int]]
 genGrid cells=[[if (x,y) `elem` cells then 1 else 0 | x<-getRangeX cells]|y<-getRangeY cells]
  
+isNeighbour :: (Int,Int) -> (Int,Int) -> Bool
+isNeighbour (x1,y1) (x2,y2) = abs (x1-x2)<1&&abs (y1-y2)<1
 --finish life function
-life :: [(Int,Int)] -> (Int,Int) -> Int
-life cells pos=  
+life :: [(Int,Int)] -> (Int,Int) -> Bool
+life cells (x,y)  
+    | live&&neighbours>1&&neighbours<4 = False
+    | not live&&neighbours==3 = False
+    | otherwise = True
+    where 
+        neighbours= length [c | c<-cells, isNeighbour (x,y) c]
+        live = (x,y) `elem` cells
 
 step :: [(Int,Int)]->[(Int,Int)]
-step cells = [life cells (x,y) | x<-getRangeX cells,y<-getRangeY cells]   
+step cells = [(x,y)  | x<-getRangeX cells,y<-getRangeY cells,life cells (x,y)]   
 
 getCells :: [[Int]]-> [(Int,Int)]
-getCells grid = [(x,y) | x<-length grid!!0, y<- length grid,grid!!y!!x==1]
+getCells grid = [(x,y) | x<-[0..(length $ grid!!0)-1], y<- [0..(length grid)-1],grid!!y!!x==1]
 
 gol :: [(Int,Int)] -> Int -> [[Int]]
-gol cells n = gol (step cells) n-1
 gol cells 0 = genGrid cells
+gol cells n = gol (step cells) (n-1)
+
 
 getGeneration :: [[Int]] -> Int -> [[Int]]
 getGeneration grid n= gol (getCells grid) n
-
